ad421b46cbe23b311b293a90ab541fd296e7db7c
나선웅 <nasw100@github.com>
2024-04-01 21:42:29 +0900

    1. [Transformer] code and test transformer 2. [Config] Generate a config file 3. [APIs] Base API Testing code

---
+++ tests/apis.py
@@ -0,0 +1,5 @@
+from fastapi.testclient import TestClient
+
+HOST = '127.0.0.1:8080'
+
+client = TestClient(HOST)
--- tests/inference.py
+++ tests/inference.py
@@ -1,15 +1,15 @@
 import pytest
 from PIL import Image
 from pathlib import Path
-from image_generator_apis.ai.inference import InferenceEngine
+from image_generator_apis.ai.transformer import ImageGeneratorTransformer
 
-inferer = InferenceEngine()
+transformer = ImageGeneratorTransformer()
 
 def test_predict() :
-    result = inferer.predict("A K-Pop band with beautiful flowers")
-    result_image = result.images[0]
-    assert type(result_image) == Image.Image
+    result = transformer.predict("A K-Pop band with beautiful flowers")
+    # result_image = result.images[0]
+    # assert type(result_image) == Image.Image
 
 @pytest.mark.parametrize('fp', ['str-test-img.png', Path(__file__).parent / 'path-test-img.png'])
-def test_save_result(fp) :
-    assert inferer.save_result(fp) == None
+def test_postprocess(fp) :
+    transformer.postprocess(fp) 
